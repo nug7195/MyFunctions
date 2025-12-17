08abc6e6a2d567b6684b87bef1752c33fcffa893
nug7195 <163369991+nug7195@users.noreply.github.com>
2025-12-17 15:20:36 -0600

    Create functions.h for string and math utilities
    Added header file for custom utility functions.

---
+++ functions.h
@@ -0,0 +1,34 @@
+// Header file for the functions I create
+#ifndef MY_FUNC_H
+#define MY_FUNC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+/*
+STRING UTILITIES
+
+1: strlcpy (size_t)
+2: strlcat (size_t)
+3: *strdup (char)
+4: stricmp (int)
+
+STRING UTILITIES
+*/
+
+/*
+MATH UTILITIES
+
+1: min (int)
+2: max (int)
+3: clamp (int)
+4: is_prime (bool)
+
+MATH UTILITIES
+*/
+
+
+
+#endif MY_FUNC_H
